a1c0db919d7bd1aa40c5d77ac09ae9caa8c106f7
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-03-17 10:52:16 -0500

    ____view

--- WIDMY/manejador_de_adendas/admin.py
+++ WIDMY/manejador_de_adendas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Registro,Adenda
+
+admin.site.register(Adenda)
+admin.site.register(Registro)
 
 # Register your models here.
--- WIDMY/manejador_de_adendas/views.py
+++ WIDMY/manejador_de_adendas/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+import json
+from django.views.decorators.csrf import csrf_exempt
+from logic import logic_adenda as la
+from logic import logic_registro as lr
+
+@csrf_exempt
+
+def adenda_view(request):
+    if request.method == 'POST':
+        adenda_dto = la.create_adenda(json.loads(request.body))
+        adenda = serializers.serialize('json',[adenda_dto])
+        return HttpResponse(adenda,'application/json')    
+    
+
+def registro_view(request):
+    regitro_dto = lr.create_registro(json.load(request.body))
+    registro = lr.create_registro('json', [registro_dto])
+    return HttpResponse(registro,'application/json')    
 
 # Create your views here.
