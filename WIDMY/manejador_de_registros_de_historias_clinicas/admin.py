1888ab93a5bc7bbd39e07cb9542d4c271c906fa9
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-03-17 12:05:15 -0500

    ________Ventana_Andmin

--- WIDMY/manejador_de_registros_de_historias_clinicas/admin.py
+++ WIDMY/manejador_de_registros_de_historias_clinicas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Historia_clinica
 
+admin.site.register(Historia_clinica)
 # Register your models here.
--- WIDMY/manejador_de_usuarios/admin.py
+++ WIDMY/manejador_de_usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario,Enfermero,Medico
 
+admin.site.register(Usuario)
+admin.site.register(Enfermero)
+admin.site.register(Medico)
 # Register your models here.
