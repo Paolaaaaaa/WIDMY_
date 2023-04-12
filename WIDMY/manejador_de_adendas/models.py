41340e5e60027d3998e7044fe3d2d8210dd6ddf2
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-04-12 18:58:44 -0500

    ___corrección relaciones

--- WIDMY/manejador_de_adendas/models.py
+++ WIDMY/manejador_de_adendas/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from manejador_de_usuarios.models import Medico 
 from manejador_de_usuarios.models import Enfermero 
+from manejador_de_registros_de_historias_clinicas.models import Historia_clinica
 
 # Create your models here.
 
+class Registro(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    tema =models.CharField( max_length=50)
+    diagnostico=models.CharField(max_length=50)
+    historia_clinica = models.ForeignKey(Historia_clinica, default= None, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return '{}'.format(self.fecha,self.tema, self.diagnostico, self.historia_clinica)
 
 class Adenda(models.Model):
     fecha = models.DateField(auto_now_add=True)
@@ -11,18 +20,9 @@ class Adenda(models.Model):
     descripcion = models.CharField(max_length=100)
     autor_medico = models.ForeignKey(Medico, default=None,on_delete=models.SET_DEFAULT)
     autor_enfermero = models.ForeignKey(Enfermero, default=None, on_delete=models.SET_DEFAULT)
-
+    registros = models.ForeignKey(Registro, default= None, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return '{}'.format(self.fecha,self.tema, self.descripcion, self.autor, self.registro)
 
 
-class Registro(models.Model):
-    fecha = models.DateTimeField(auto_now_add=True)
-    tema =models.CharField( max_length=50)
-    diagnostico=models.CharField(max_length=50)
-    adenda= models.ForeignKey(Adenda, default=None, on_delete=models.CASCADE)
-
-
-    def __str__(self) -> str:
-        return '{}'.format(self.fecha,self.tema, self.diagnostico, self.historia_clinica)
--- WIDMY/manejador_de_registros_de_historias_clinicas/models.py
+++ WIDMY/manejador_de_registros_de_historias_clinicas/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from manejador_de_pacientes.models import Paciente
 from manejador_de_usuarios.models import Medico 
-from manejador_de_adendas.models import Registro
 
 # Create your models here.
 class Historia_clinica(models.Model):
     id_historia_clinica=models.BigIntegerField()
     paciente = models.ForeignKey(Paciente, default=None, on_delete=models.CASCADE)
-    autor = models.ForeignKey(Medico, default=None, on_delete=models.CASCADE)
-    registros = models.ForeignKey(Registro, default=None, on_delete=models.CASCADE, null=True)
     diagnostico = models.CharField(max_length=100, default=None)
     tema = models.CharField(max_length=100, default=None)
-    fecha = models.CharField(max_length=100,default=None)
+    fecha = models.DateField(default=None)
     medicamento = models.CharField(max_length=100, default=None)
+    autor_medico = models.ForeignKey(Medico, default=None,on_delete=models.SET_DEFAULT)
+
+
+    def __str__(self) -> str:
+        return '{}'.format(self.id_historia_clinica,self.diagnostico, self.tema, self.fecha,self.medicamento)
+
     #falta poner registros 
