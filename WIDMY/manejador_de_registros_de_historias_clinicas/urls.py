3d5969536e84e877da0c39626eaf90b5b523ca69
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-04-14 19:47:47 -0500

    naURLD

--- WIDMY/manejador_de_registros_de_historias_clinicas/urls.py
+++ WIDMY/manejador_de_registros_de_historias_clinicas/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('historia_clinica/',views.historia_clinica_view, name='historia_clinica_view'),
     path('historia_clinica/<int:pk>/',views.historia_clinica_get_one, name='historia_clinica_get_one'),
-    path('historia_clinica/<int:pk>/registro',views.historia_clinica_get_one, name='historia_clinica_get_one'),
 
 
 ]
