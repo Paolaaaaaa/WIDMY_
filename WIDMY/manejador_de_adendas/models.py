85ccbdb9f36e1905e99543a9ef49c0328e1374b8
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-05-20 21:49:53 -0500

    corrección_html_cifrado

--- WIDMY/manejador_de_adendas/models.py
+++ WIDMY/manejador_de_adendas/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django_cryptography.fields import encrypt
+
 from manejador_de_usuarios.models import Medico 
 from manejador_de_usuarios.models import Enfermero 
 from manejador_de_registros_de_historias_clinicas.models import Historia_clinica
-from django_cryptography.fields import encrypt
 # Create your models here.
 
 class Ips(models.Model):
@@ -22,14 +23,13 @@ class Servicio(models.Model):
 
 class Adenda(models.Model):
     historia_clinica = models.ForeignKey(Historia_clinica, default= -1, on_delete=models.SET_DEFAULT)
-    #diagnostico = models.CharField(max_length=50, default=None)
     diagnostico = encrypt(models.CharField(max_length=50, default=None))
     fecha = models.DateField(auto_now_add=True)
-    tema= models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100)# comentarios 
+    tema= encrypt(models.CharField(max_length=50, default= None))
+    descripcion = encrypt(models.CharField(max_length=100, default=None))# comentarios 
     autor_medico = models.ForeignKey(Medico, default=None,on_delete=models.SET_DEFAULT, blank= True, null= True)
     autor_enfermero = models.ForeignKey(Enfermero, default=None, on_delete=models.SET_DEFAULT, blank= True,null= True)
-    servicio = models.ForeignKey(Servicio, default=None, blank=True, on_delete=models.CASCADE)
+    servicio = (models.ForeignKey(Servicio, default=None, blank=True, on_delete=models.CASCADE))
     def __str__(self) -> str:
         return '{}'.format(self.tema, self.descripcion, self.diagnostico, self.autor_enfermero, self.autor_medico, self.servicio, self.historia_clinica)
     
