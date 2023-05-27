7b19e26fb0220790d80011b9779064592b4a3fe1
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-05-27 01:06:43 -0500

    correct

--- Manejador_de_adendas/WIDMY/settings.py
+++ Manejador_de_adendas/WIDMY/settings.py
@@ -73,28 +73,28 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = 'WIDMY.wsgi.application'
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'adenda_db',   
         'USER': 'user_db', 
         'PASSWORD': 'isis2503',
-        'HOST': '10.128.0.4',         
+        'HOST': '10.182.0.14',         
         'PORT': '5432',       
         }
 }
-"""
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
