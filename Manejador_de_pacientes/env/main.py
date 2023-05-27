2067fce47ed87533e9ba8316ed603c9fb8669201
Santiago Ayala <UniandesLogin@uniandes.edu.co>
2023-05-26 20:09:33 -0500

    ola

--- Manejador_de_pacientes/env/main.py
+++ Manejador_de_pacientes/env/main.py
@@ -1,21 +1,34 @@
 from fastapi import FastAPI, HTTPException, Request
 from typing import Union
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import views 
 
 app = FastAPI()
 
-app.data = []
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.post("/pacientes/")
+@app.post("/pacientepost/")
 async def crear_paciente(request: Request):
-    paciente = await request.json()
-    paciente_id = paciente.get("id")
-    paciente_nombre = paciente.get("nombre")
-    # Aquí puedes procesar los datos recibidos y almacenarlos en una base de datos, por ejemplo.
-    # Asegúrate de validar los datos y realizar las operaciones necesarias.
-    # ...
-    return {"message": f"Paciente creado: ID: {paciente_id}, Nombre: {paciente_nombre}"}
+    paciente = views.create_item(get_db ,request)
+        
+    return paciente
 
+@app.get("/paciente/id")
+async def obtener_paciente(paciente_id: int):
+    paciente = views.get_item(paciente_id)
+    if paciente is None:
+        raise HTTPException(status_code=404, detail="El paciente no existe")
+    return paciente
+
+# Replace 'username', 'password', 'host', 'port', and 'database_name' with your database credentials
+DATABASE_URL = "postgresql://db_user:isis2503@10.182.0.15:5432/pacientes_db"
+#34.125.215.124
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
